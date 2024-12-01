8d2f8b349876f233e1adff85f05f2709fc376dd6
ThomasPszowski <162191217+ThomasPszowski@users.noreply.github.com>
2024-12-01 19:23:03 +0100

    osobny plik na pinout

--- display/display.ino
+++ display/display.ino
@@ -1,11 +1,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7735.h>
 #include "doofenshmirtz.h"
-// Define pins
-#define TFT_CS     10
-#define TFT_DC     9
-#define TFT_RST    8
-#define TFT_BL     7
+#include "pinout.h"
 
 // Create the display object
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
