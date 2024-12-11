c128b2ef1b7e123967544810b4f53c0839d40988
Moxiram <rafemac202@gmail.com>
2024-12-11 17:45:06 +0100

    added UpadateLED function

--- LED/LED.h
+++ LED/LED.h
@@ -26,13 +26,39 @@ inline void OrangeOff() {
   digitalWrite(ORANGE_DIODE, LOW);
 }
 
-void UpdateLED(bool partyMode, float interval_modifier) { // 0 < interval_modifier < 1
+void UpdateLED(bool partyMode, float interval_modifier) { // 0 < interval_modifier <= 1
+  unsigned long currentMillis = millis();
+
   if (partyMode) {
+    
     int pink_interval_tmp = pink_interval * interval_modifier;
     int orange_interval_tmp = orange_interval * interval_modifier;
-  }
-  else {
-    PinkOff(); OrangeOff();
+
+    
+    if (currentMillis - last_pink_timestamp >= pink_interval_tmp) {
+      last_pink_timestamp = currentMillis;
+      
+      if (digitalRead(PINK_DIODE) == HIGH) {
+        PinkOff();
+      } else {
+        PinkOn();
+      }
+    }
+
+    
+    if (currentMillis - last_orange_timestamp >= orange_interval_tmp) {
+      last_orange_timestamp = currentMillis;
+      
+      if (digitalRead(ORANGE_DIODE) == HIGH) {
+        OrangeOff();
+      } else {
+        OrangeOn();
+      }
+    }
+  } else {
+    
+    PinkOff();
+    OrangeOff();
   }
 }
 
