14b758fe64d04b728252f1503e847bc99bd6faca
ThomasPszowski <162191217+ThomasPszowski@users.noreply.github.com>
2025-02-10 19:57:11 +0100

    merge systemu głównego

--- pinout.h
+++ pinout.h
@@ -7,4 +7,21 @@
 #define TFT_BL     7 // backlight control
 
 // Continuous servo FS90R pinout
-#define SERVO_PIN  6 // servo control
+#define SERVO_PIN  6 
+
+// Iduino sound sensor
+#define SOUND_PIN A0
+
+// Diodes/
+#define PINK_DIODE 12
+#define ORANGE_DIODE 5
+
+// OLED
+#define OLED_SDA A4
+#define OLED_SCL A5
+
+// Vixa Mode
+#define VIXA_PIN 4
+
+// POTENTIOMETER
+#define POT_PIN A3
--- servo/servo.h
+++ servo/servo.h
@@ -1,13 +1,17 @@
-#include "pinout.h"
+#pragma once
 #include <Servo.h>
 
 Servo myServo; // Obiekt serwa
 
-void setup() {
+void setupServo() {
   myServo.attach(SERVO_PIN); // Podłącz serwo do pinu cyfrowego 9
-  myServo.write(95);
 }
 
-void loop() {
+void SpinServo() {
+  myServo.write(80); 
   
 }
+void StopServo() {
+  myServo.write(90); 
+}
+
